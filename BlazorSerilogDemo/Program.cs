5f649ed3790ab1fd5b434fb9dba11451e5507b58
Kent Riggins <kent13@boohoo.us>
2021-03-04 20:40:30 -0500

    Added detects minimum log level to text. Keeping the LoggingLevelSwitch in LoggerService.

--- BlazorSerilogDemo/Program.cs
+++ BlazorSerilogDemo/Program.cs
@@ -24,12 +24,11 @@ namespace BlazorSerilogDemo
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
             builder.Services.AddSingleton<LoggerService>();
 
-            var host = builder.Build();           
-
-            var levelSwitch = new LoggingLevelSwitch();
-            levelSwitch.MinimumLevel = LogEventLevel.Verbose;
+            var host = builder.Build();
+            var loggerService = host.Services.GetService<LoggerService>();
+            loggerService.LevelSwitch.MinimumLevel = LogEventLevel.Warning;
             Log.Logger = new LoggerConfiguration()
-                .MinimumLevel.ControlledBy(levelSwitch)
+                .MinimumLevel.ControlledBy(loggerService.LevelSwitch)
                 .Enrich.WithProperty("InstanceId", Guid.NewGuid().ToString("n"))
                 .WriteTo.LoggerSink(host.Services.GetRequiredService<LoggerService>()) // the Blazor custome cachs log
                 .WriteTo.BrowserConsole() // browser console
--- BlazorSerilogDemo/Services/LoggerService.cs
+++ BlazorSerilogDemo/Services/LoggerService.cs
@@ -1,4 +1,7 @@
 ﻿using BlazorSerilogDemo.Tools;
+using Serilog;
+using Serilog.Core;
+using Serilog.Events;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,10 +12,11 @@ namespace BlazorSerilogDemo.Services
     public class LoggerService
     {
         private List<LogItem> _log = new List<LogItem>();
+        public LoggingLevelSwitch LevelSwitch = new LoggingLevelSwitch();
 
         public LoggerService()
         {
-
+            LevelSwitch.MinimumLevel = LogEventLevel.Verbose;
         }
 
         public void Add(LogItem logItem)
@@ -25,6 +29,20 @@ namespace BlazorSerilogDemo.Services
             return _log.TakeLastReverse(numberToGet);
         }
 
+        public string GetMinimumLogLevel()
+        {
+            string result = "I don't Know";
+            if (Log.IsEnabled(LogEventLevel.Verbose)) { result = "Verbose"; }
+            else if (Log.IsEnabled(LogEventLevel.Debug)) { result = "Debug"; }
+            else if (Log.IsEnabled(LogEventLevel.Warning)) { result = "Warning"; }
+            else if (Log.IsEnabled(LogEventLevel.Information)) { result = "Information"; }
+            else if (Log.IsEnabled(LogEventLevel.Error)) { result = "Error"; }
+            else if (Log.IsEnabled(LogEventLevel.Fatal)) { result = "Fatal"; }
+
+
+
+            return result;
+        }
     }
 
 
